555b91130537306ad67e61fb59eaf319b454345a
josfam <josephamukun@tutanota.com>
2024-07-25 16:22:54 +0000

    Remove `var` from variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
-    task;
     const task2 = false;
-    task2;
   }
 
   return [task, task2];
