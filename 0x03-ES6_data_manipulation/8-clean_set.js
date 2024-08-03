7eb0f1ab435d674e797025b4f2cf00597d389811
josfam <jamukun@proton.me>
2024-08-03 18:14:31 +0300

    Implement `8. Clean set8. Clean set8. Clean set8. Clean set8. Clean set8. Clean set8. Clean set8. Clean set`

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,14 @@
 export default function cleanSet(set, startString) {
-  
+  if (!startString) {
+    return '';
+  }
+
+  const parts = [];
+  for (const word of set) {
+    if (word.startsWith(startString)) {
+      const startIndex = startString.length;
+      parts.push(word.slice(startIndex));
+    }
+  }
+  return parts.join('-');
 }
