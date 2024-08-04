5e3c64c8ea52d87988706386146297a6baad7c6b
josfam <jamukun@proton.me>
2024-08-04 10:11:02 +0300

    Implement `11. Weak link data structure`

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+export const weakMap = new WeakMap();
+
+export const queryAPI = function queryAPI(endpoint) {
+  // add the endpoint to the map
+  if (!(weakMap.has(endpoint))) {
+    weakMap.set(endpoint, 1);
+  } else {
+    // update the count, and check for high load
+    const count = weakMap.get(endpoint);
+    weakMap.set(endpoint, count + 1);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+};
