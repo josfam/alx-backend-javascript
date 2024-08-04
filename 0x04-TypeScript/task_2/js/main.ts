f964dc2c6a450d415b6d197f6442f4b48512e2d3
josfam <jamukun@proton.me>
2024-08-04 21:38:44 +0300

    Fix: Return the interfaces, not the classes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,7 +34,9 @@ export class Teacher implements TeacherInterface {
 	}
 }
 
-export function createEmployee (salary: number | string): Teacher | Director {
+export function createEmployee (
+	salary: number | string): TeacherInterface | DirectorInterface {
+
 	if (typeof salary === 'number' && salary < 500) {
 		return new Teacher;
 	}
