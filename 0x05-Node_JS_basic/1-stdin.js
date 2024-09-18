1dd1aa028093362b0e765708aa828f90b20c0230
josfam <jamukun@proton.me>
2024-09-18 14:32:06 +0300

    Fix format of output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,35 @@
 const readline = require('node:readline');
 
-// setup input and output
+// setup input and output for readline interface
 const rlInterface = readline.Interface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Ask the user for their name, and print it
-rlInterface.question(
-  'Welcome to Holberton School, what is your name?\n', (name) => {
+// checks if we are in interactive mode (terminal input)
+const isInteractive = process.stdin.isTTY;
+
+if (isInteractive) {
+  // Ask the user for their name, and print it
+  rlInterface.question(
+    'Welcome to Holberton School, what is your name?\n', (name) => {
+      console.log(`Your name is: ${name}`);
+      rlInterface.close();
+    },
+  );
+} else {
+  // data got via a pipe
+  process.stdin.on('data', (data) => {
+    console.log('Welcome to Holberton School, what is your name?\n');
+    const name = data.toString().trim();
     console.log(`Your name is: ${name}`);
     rlInterface.close();
-  },
-);
+  });
+}
 
 // Print a message when the program closes normally or prematurely with CTRL + C
 rlInterface.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+  if (!isInteractive) {
+    console.log('This important software is now closing');
+  }
 });
