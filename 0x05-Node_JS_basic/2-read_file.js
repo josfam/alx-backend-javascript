ae562691375c156d8e5c9e932f110fa5e3d741bf
josfam <jamukun@proton.me>
2024-09-18 17:45:57 +0300

    Implement `2. Reading a file synchronously with Node JS`

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  let studentCount = 0;
+  let text = null;
+  const fieldList = {}; // stores fields, and which students belong in them
+  try {
+    text = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  const rows = text.split('\n').slice(1); // skip the header row
+  for (const row of rows) {
+    if (row) {
+      studentCount += 1;
+      const [firstName, , , field] = row.split(',');
+      // add the name of this student to fields
+      if (!fieldList[field]) {
+        fieldList[field] = [];
+        fieldList[field].push(firstName);
+      } else {
+        fieldList[field].push(firstName);
+      }
+    }
+  }
+  console.log(`Number of students: ${studentCount}`);
+  for (const [field, entry] of Object.entries(fieldList)) {
+    const fieldCount = entry.length;
+    const fieldMembers = entry.join(', ');
+    console.log(`Number of students in ${field}: ${fieldCount}. List: ${fieldMembers}`);
+  }
+}
+
+module.exports = countStudents;
