2dffafb37a28d05e75166b8be6dddffeab4ce1b3
josfam <jamukun@proton.me>
2024-09-21 01:12:35 +0300

    Add 0 / any number test case

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -20,7 +20,7 @@ describe('round test with types for SUBTRACT', () => {
 describe('round test with types for DIVIDE', () => {
   it('divides correctly when given DIVIDE type', () => {
     assert.strictEqual(calculateWithType('DIVIDE', 0.4, 0.4), 'Error');
-    assert.strictEqual(calculateWithType('DIVIDE', 0.5, 0.5), 1);
+    assert.strictEqual(calculateWithType('DIVIDE', 0, 0.5), 0);
     assert.strictEqual(calculateWithType('DIVIDE', 0.5, 2), 0.5);
     assert.strictEqual(calculateWithType('DIVIDE', 6, 2.4), 3);
   });
