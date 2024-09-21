a0647203a40d3c8b258f62abf2d1a21d7517ba22
josfam <jamukun@proton.me>
2024-09-21 18:59:27 +0300

    Add test for denominator being 0

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -19,10 +19,16 @@ describe('round test with types for SUBTRACT', () => {
 
 describe('round test with types for DIVIDE', () => {
   it('divides correctly when given DIVIDE type', () => {
-    assert.strictEqual(calculateWithType('DIVIDE', 0.4, 0.4), 'Error');
+    assert.strictEqual(calculateWithType('DIVIDE', 0.4, 0.4), 0);
     assert.strictEqual(calculateWithType('DIVIDE', 0, 0.5), 0);
     assert.strictEqual(calculateWithType('DIVIDE', -4, 16), -0.25)
     assert.strictEqual(calculateWithType('DIVIDE', 0.5, 2), 0.5);
     assert.strictEqual(calculateWithType('DIVIDE', 6, 2.4), 3);
+  });
+
+  it('shows error when b is 0', () => {
+    assert.strictEqual(calculateWithType(('DIVIDE', 4, 0.4), 'Error'));
+    assert.strictEqual(calculateWithType(('DIVIDE', 1, 0), 'Error'));
+    assert.strictEqual(calculateWithType(('DIVIDE', -1.5, 0.0), 'Error'));
   });
 });
